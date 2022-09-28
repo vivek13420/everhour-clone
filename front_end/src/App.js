c7b140a0246396fcbe2735f3019d766b34274a53
Alisha <alishamohasin85@gmail.com>
2022-09-28 22:19:27 +0530

    sign up components

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,13 +2,17 @@
 import Navbar from './Components/Navbar';
 import Allroutes from './Routes/Allroutes';
 import Log_Route from "./Components/Log_Route/Log_Route"
+import Signup from './Pages/Signup';
+import Onbording from './Pages/Onbording';
+import Createteam from './Pages/Createteamform';
 
 function App() {
   return (
     <div>
-      {/* <Allroutes/>
-      <Navbar/> */}
-<Log_Route/>
+       <Allroutes/>
+      {/* <Navbar/>  */}
+       {/* <Log_Route/>  */}
+     
 
 
 
