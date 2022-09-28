4d65faa5b63978cbed462567508b1ae1505e1d39
pravhatray <pravhatray@gmail.com>
2022-09-28 23:09:47 +0530

    ad

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -7,7 +7,7 @@ import Log_Route from "./Components/Log_Route/Log_Route"
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <Allroutes />
       {/* <Navbar/>  */}
       {/* <Log_Route/>  */}
