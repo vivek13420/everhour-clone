40d02f303cb877cde94094c74ca058b7b6afcbcf
Alisha <alishamohasin85@gmail.com>
2022-09-29 15:48:41 +0530

    updated

--- backend/src/Components/Users/Users.model.js
+++ backend/src/Components/Users/Users.model.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String,max:8,required:true },
+  password: { type: String, max: 8, required: true },
+  role: { type: String, required: true, enum: ["admin", "client", "employee"] }
+  
 
 });
 
