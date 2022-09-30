9c322b50051318ed9b1ff7464a5360c301ecedb0
Alisha <alishamohasin85@gmail.com>
2022-09-30 10:53:51 +0530

    updated

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -8,11 +8,8 @@ function App() {
       <Allroutes />
       {/* <Navbar/>  */}
       {/* <Log_Route/>  */}
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 4d824f49f39f4b7aff8db3d66409cb1c9a32ad23
     </div>
   );
 }
